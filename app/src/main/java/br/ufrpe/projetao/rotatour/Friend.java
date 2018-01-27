cbf24f50af3d9343e84c8e5d5a8d2b03700893ad
victor-silva <victor9300@gmail.com>
2018-01-27 16:48:25 -0300

    ajuste strings

---
+++ app/src/main/java/br/ufrpe/projetao/rotatour/Friend.java
@@ -0,0 +1,8 @@
+package br.ufrpe.projetao.rotatour;
+
+/**
+ * Created by Victor Alexandre on 1/27/2018.
+ */
+
+public class Friend {
+}
---
+++ app/src/main/java/br/ufrpe/projetao/rotatour/adapters/FriendsAdapter.java
@@ -0,0 +1,8 @@
+package br.ufrpe.projetao.rotatour.adapters;
+
+/**
+ * Created by Victor Alexandre on 1/27/2018.
+ */
+
+public class FriendsAdapter {
+}
